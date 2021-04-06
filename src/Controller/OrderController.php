7f990135fbc72717fbff4a136b90c75e497275b4
Hugo Givo <hugolive84@gmail.com>
2021-04-06 14:29:24 +0200

    Page : commande et suivi administration

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -91,7 +91,7 @@ class OrderController extends AbstractController
                 $this->entityManager->persist($orderDetails);
             }
 
-            //$this->entityManager->flush();
+            $this->entityManager->flush();
             
             return $this->render('order/add.html.twig', [
                 'cart' => $cart->getFull(),
