c454e4152dccdcb1f919d00bccafffac4ebf98bf
Hugo Givo <hugolive84@gmail.com>
2021-04-27 15:22:10 +0200

    Mettre des produits à la une pour les afficher sur la homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/", name="home")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $products = $this->entityManager->getRepository(Product::class)->findByIsBest(1);
+
+        return $this->render('home/index.html.twig', [
+            'products' => $products
+        ]);
     }
 }
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -49,6 +49,7 @@ class ProductController extends AbstractController
     public function show($slug)
     {
         $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
+        $products = $this->entityManager->getRepository(Product::class)->findByIsBest(1);
  
 
         if (!$product) {
@@ -56,7 +57,8 @@ class ProductController extends AbstractController
         }
 
         return $this->render('product/show.html.twig', [
-            'product' => $product
+            'product' => $product,
+            'products' => $products
         ]);
     }
 }
